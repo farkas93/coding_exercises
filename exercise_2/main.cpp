3a99c7931f6b049766e276f864a438b96851af52
farkas93 <zsombor.kalotay@gmx.ch>
2022-04-17 09:50:50 +0200

    solve exercise 2

--- exercise_2/main.cpp
+++ exercise_2/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-vector<int> myarray = {3, 5, 0, 1, 0, 7, 4};
+vector<int> myarray = {3, 5, 0, 1, 0, -10, 0, 0, 7, 4};
 
 void print_solution(vector<int> arr){
   cout << "Solution is [ ";
@@ -23,6 +23,7 @@ void print_solution(vector<int> arr){
  * @brief Pushing all the 0 by first copying the array into a deque -> putting all the zeros to the front. O(n), 
  * since push_back and push_front both O(1), copying array to deque is O(n). Copying deque back to an array is O(n) too.
  * Total complexity: O(n)
+ * Memory complexity: O(n)
  */
 void naive_method(){
   deque<int> my_deque;
@@ -41,7 +42,31 @@ void naive_method(){
   print_solution(new_array);
 }
 
-int main(int argc, char* argv[]){
-  naive_method();
+/**
+ * @brief Pushing all the 0 by in-place comparison of the elements to the front. O(n)
+ * Total complexity: O(n)
+ * Memory complexity: O(1)
+ */
+void inplace_method(){
+  int copy_to_ind = myarray.size()-1;
+  for(int i = myarray.size()-1; i >= 0; --i){
+    if(myarray[i] == 0){
+      if(myarray[i-1] != 0){
+        //swap
+        myarray[copy_to_ind] = myarray[i-1];
+        myarray[i-1] = 0; 
+      }
+      else continue;
 
+    }
+    --copy_to_ind;
+  }
+}
+
+int main(int argc, char* argv[]){
+  
+  print_solution(myarray);
+  naive_method();
+  inplace_method();  
+  print_solution(myarray);
 }
